59f3e934c9cd5f0b26f0a11df4633df6be215b58
Lwazismkhwanazi <sibusisomkhwanazi59@gmail.com>
2020-01-29 13:29:17 +0000

    adding header to our app and shop and about nav links

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -3,10 +3,12 @@ import Directory from "../../componets/directory/directory.componet";
 import "./homepage.styles.scss";
 import ShopPage from '../shop-page/shop.component'
 import { Route, Switch } from "react-router-dom";
+import Header from '../../componets/header-component/header.component'
 
 export default function Homepage() {
   return (
     <div className="homepage">
+      <Header/>
       <Switch>
         <Route exact path= '/'  component={Directory} />
         <Route path= '/shop' component={ShopPage} />
